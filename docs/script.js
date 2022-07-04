2f3f8eafa22263eac69919a2256e295af00eeb9d
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-07-04 16:31:12 +0530

    ..

--- docs/script.js
+++ docs/script.js
@@ -1,17 +1,24 @@
-var cube = document.querySelector('.cube');
-var radioGroup = document.querySelector('.radio-group');
-var currentClass = '';
-
-function changeSide() {
-  var checkedRadio = radioGroup.querySelector(':checked');
-  var showClass = 'show-' + checkedRadio.value;
-  if ( currentClass ) {
-    cube.classList.remove( currentClass );
+var cube = document.getElementById("cube")
+document.onclick = (e) => {
+  alert(e.target.id)
+  switch (e.target.id) {
+    case 'top':
+      cube.style.transform = "rotateX(-90deg)";
+      break;
+    case 'bottom':
+      cube.style.transform = "rotateX(90deg)";
+      break;
+    case 'left':
+      cube.style.transform = "rotateY(90deg)";
+      break;
+    case 'right':
+      cube.style.transform = "rotateY(-90deg)";
+      break;
+    case 'front':
+      cube.style.transform = "rotateZ(-90deg)";
+      break;
+    case 'back':
+      cube.style.transform = "rotateX(180deg)";
+      break;
   }
-  cube.classList.add( showClass );
-  currentClass = showClass;
 }
-// set initial side
-changeSide();
-
-radioGroup.addEventListener( 'change', changeSide );
